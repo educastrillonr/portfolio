bbe904bc860b65c9cf8bab3466577a33be49f377
Edu <educastrillonr@gmail.com>
2019-08-23 23:01:39 +0100

    links

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
 import styles from "./Header.module.scss";
+import { Link } from "@reach/router";
 
 const Header: React.FC = () => {
-  return <header className={styles.header}>header</header>;
+  // const isActive: boolean = ({ isCurrent }: ) => {
+  //   return isCurrent ? { style: "background: rgb(88, 164, 176);" } : null;
+  // };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo} />
+      <nav>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/about-me">About me</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
