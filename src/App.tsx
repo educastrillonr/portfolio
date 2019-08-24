d0b066bd61154f78e644c6844014222faf6068af
Edu <educastrillonr@gmail.com>
2019-08-24 23:10:30 +0100

    removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
         <Social />
         <Routes />
       </section>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -2,7 +2,11 @@ import * as React from "react";
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
-  return <footer className={styles.footer}>aa</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>Built and designed by Eduardo Castrillón Romero</p>
+    </footer>
+  );
 };
 
 export default Footer;
