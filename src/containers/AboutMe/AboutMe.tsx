b0edb0731437434ec20a6505b4b8b8c75e3c656e
Eduardo Romero <eduardo.romero@Nology.local>
2019-09-03 15:47:56 +0100

    about me

--- src/containers/AboutMe/AboutMe.tsx
+++ src/containers/AboutMe/AboutMe.tsx
@@ -9,22 +9,21 @@ const AboutMe: React.FC = () => {
         <div className={styles.titles}>
           <h2>Eduardo Castrillon Romero</h2>
           <h3>Code & Me</h3>
+          <p>
+            Having always loved tech, I enrolled on a CS Degree in Spain back
+            when I was 18. Eventually, I moved on to working hospitality full
+            time.
+          </p>
         </div>
         <img src={avatar} alt="Edu" />
       </header>
       <article>
         <p>
-          Having always loved tech, I enrolled on a CS Degree in Spain back when
-          I was 18. Unfortunately, it wasn't what I thought it'd be.
-        </p>
-        <p>
-          While I wondered what to do with myself, I started working at a bar,
-          which soon became a full time position and led me to actually enjoy
-          working hospitality. All the while, I always had a feeling I wanted to
-          work in something related to technology. So I took this thought and
-          moved to Bristol, still working hospitality to sustain myself. I
-          eventually decided change couldn't happen while I still worked in bars
-          full time, so I commited to a full time coding bootcamp called{" "}
+          Going into hospitality taught me a lot about working in teams. All the
+          while, I always had a thought in the back of my mind that wanted to
+          work with technology. Eventually I decided to get back to coding on my
+          own, but I wasn't getting as much from that as I hoped, so I commited
+          to a full time coding bootcamp called{" "}
           <a
             rel="noopener noreferrer"
             href="https://www.nology.io/"
