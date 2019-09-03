5f2d0f14c542762a909f5536364755771a7905de
Eduardo Romero <eduardo.romero@Nology.local>
2019-09-03 08:50:53 +0100

    hide about me

--- src/containers/Routes/Routes.tsx
+++ src/containers/Routes/Routes.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Router, Redirect } from "@reach/router";
 import Tiles from "../Tiles/Tiles";
-import AboutMe from "../AboutMe/AboutMe";
+// import AboutMe from "../AboutMe/AboutMe";
 
 const Routes: React.FC = () => {
   return (
     <Router>
       <Redirect noThrow from="/" to="/portfolio" />
       <Tiles path="/portfolio" />
-      <AboutMe path="/about-me" />
+      {/* <AboutMe path="/about-me" /> */}
     </Router>
   );
 };
